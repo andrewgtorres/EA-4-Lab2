ca34dfa315d9dad310cc25411ff3491faceb3324
andrewgtorres <73441342+andrewgtorres@users.noreply.github.com>
2021-10-17 17:23:56 -0500

    Update ODE45test.m

--- ODE45test.m
+++ ODE45test.m
@@ -1,1 +1,15 @@
-
+% ODE45 test.m
+%
+% declaring global variables a,B
+global a
+a=1;
+global B
+B=2;
+% declaring initial P and final t
+i_init=0.01;
+tfin=15;
+% Matlab default tolerance
+tol = 1*10^(-7); 
+options=odeset('RelTol',tol,'AbsTol',tol);
+[t,i] = ode45(@Eq5ode,[0,tfin],i_init, options);
+plot(t,i, 'black');
